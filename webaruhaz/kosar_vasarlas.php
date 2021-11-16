44795681fabaf53a4222a83425790725d60934c8
Jakab Balázs Erik <jakabb1@kkszki.hu>
2021-11-16 10:37:37 +0100

    urlap hozzaadasa

--- webaruhaz/kosar_vasarlas.php
+++ webaruhaz/kosar_vasarlas.php
@@ -79,14 +79,84 @@ while ($sor = mysqli_fetch_array($eredmeny,MYSQLI_ASSOC))
 		}
 		
 		print("</tr>");
-	}		
-print("</table>\n");
 
-mysqli_close($kapcsolat);
+	}
+		print("</table>\n");
 
-?>
-<br>
-</div>
-<?php include 'footer.php';?>
-</body>
-</html>
+		print ("<br>
+		<hr>
+		<h2 align=\"center\">A megrendelés adatai:</h2>"
+		);
+		
+		//print ("A mennyiseg: ".$mennyiseg);
+		//print (" A termék id: ".$termek_id);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		print ("<br>
+		<form name=\"form\" method=\"post\" action=\"folyamatban1.php\">
+			<table width=\"700\" align=\"center\" class=\"table table-bordered\">
+			  <tr>
+				<td><label>A megrendelt termék neve:</label></td>
+				<td>$termeknev</td>
+			  </tr>
+			  <tr>
+				<td><label>A megrendelt termék kategoriája:</label></td>
+				<td>$kategoria</td>
+			  </tr>
+			  <tr>
+				<td><label>A megrendelt termék ára:</label></td>
+				<td>$ar Ft/db</td>
+			  </tr>
+			  ");
+		mysqli_close($kapcsolat);
+		?>	  
+			  
+			  <tr>
+				<td><label>A megrendelt termék mennyisége:</label></td>
+				<td><input name="mtm" type="text" id="mtm" size="3" /> db</td>
+			  </tr>
+			  <tr>
+				<td><label>A megrendelõ neve:</label></td>
+				<td><input name="mn" type="text" id="mn" size="90" /></td>
+			  </tr>
+			  <tr>
+				<td><label for="email">A megrendelõ e-mail címe:</label></td>
+				<td><input type="email" class="_form-control" placeholder="valaki@pelda.com" id="email" size="30" ></td>
+			  </tr>
+			  <tr>
+				<td><label>A megrendelõ lakcíme:</label></td>
+				<td><input name="ml" type="text" id="ml" size="90" /></td>
+			  </tr>
+			  <tr>
+				<td><label>Szállítási cím:</label></td>
+				<td><input name="szc" type="text" id="szc" size="90" /></td>
+			  </tr>
+			  <tr>
+				<td><label>Fizetési mód:</label></td>
+				<td>Kiszállításkor utánvétellel történik.</td>
+			  </tr>
+		
+			</table>
+			<p align="center">
+			  <button type="submit"> 
+			  <div align="center">Megrendelem</div>
+			  </button></p>
+		</form>
+			
+			
+		</div>
+		<div class="container" align="center">
+		<hr>
+		<br>
+		</div>
+		<?php include 'footer.php';?>
+		</body>
+		</html>
